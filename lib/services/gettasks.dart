ed8edf9313fe27f30d366dd2db21bce04e0d5cf4
Toe Zayar Moe <toezayarmoe@gmail.com>
2023-03-25 11:05:29 -0400

    Change Style

--- lib/services/gettasks.dart
+++ lib/services/gettasks.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:time_control/models/task.dart';
 
+String url = "https://hrbackend.cyclic.app/api/GetTasks";
+
 Future<List<Task>> getTasks() async {
-  final response = await http.get(Uri.parse("https://hrbackend.cyclic.app/api/GetTasks"));
-  if (response.statusCode == 200){
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
     List<dynamic> jsonResponse = jsonDecode(response.body);
     return jsonResponse.map((json) => Task.fromJson(json)).toList();
-  } else {
-    throw Exception('Failed to load tasks');
   }
+  throw Exception('Failed to load tasks');
 }
