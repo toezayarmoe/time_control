9155c3232e00cdcf4c199c66558301e0cdea9983
Toe Zayar Moe <toezayarmoe@gmail.com>
2023-03-25 11:06:21 -0400

    get worker

---
+++ lib/services/getworker.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:time_control/models/worker.dart';
+import 'package:http/http.dart' as http;
+
+String url = "https://hrbackend.cyclic.app/api/GetTasks";
+
+Future<List<Worker>> getWorker() async {
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    List<dynamic> jsonResponse = jsonDecode(response.body);
+    return jsonResponse.map((json) => Worker.fromJson(json)).toList();
+  }
+  throw Exception("Fail to get Wroker");
+}
